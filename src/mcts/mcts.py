55ba8b67511afc073ee8ddb2f4e6bfe67927e317
Arachne0 <1022lsh@naver.com>
2025-07-23 00:04:46 +0900

    sh edited

--- src/mcts/mcts.py
+++ src/mcts/mcts.py
@@ -123,67 +123,92 @@ class MCTS(object):
             action, node = node.select(self._c_puct)
             obs, reward, terminated, info = env.step(action)
 
-        if self.rl_model in "DQN":
-            available, action_probs, leaf_value = self._policy(env)
-            if len(available) > 0:
-                # Action probabilities need to be created as a one-hot vector
-                action_probs = np.zeros_like(action_probs)
-
-                # argmax only for the sensible moves
-                leaf_value_ = leaf_value.cpu().numpy()
-                masked_leaf_value = np.zeros_like(leaf_value_)
-                masked_leaf_value[available] = leaf_value_[available]
-
-                idx_max = available[np.argmax(masked_leaf_value[available])]
-                action_probs[idx_max] = 1
-
-                # add epsilon to sensible moves and discount as much as it from the action_probs[idx_max]
-                action_probs[available] += self.epsilon / len(available)
-                action_probs[idx_max] -= self.epsilon
-
-                """ use oracle """
-                """ calculate next node.select's node._Q """
-                # leaf_temp = node._Q + (leaf_value - node._Q)/ (node._n_visits+1)
-                # # do we need to calculate next node._u?
-                # leaf_value = leaf_value_[leaf_temp.argmax()]
-
-                """ use bellman expection to cal state value """
-                # leaf_value = (leaf_value_*action_probs).mean()
-
-                """ use bellman optimality to cal state value """
-                leaf_value = masked_leaf_value[available].max()
-            else:
-                # Even if len(available) == 0, there is no issue because the leaf value will eventually be set to 0.
-                leaf_value = leaf_value.max()
-            action_probs = zip(available, action_probs[available])
-
-        elif self.rl_model == "QRDQN":
+        if self.rl_model in ["DQN", "QRDQN"]:
             available, action_probs, leaf_value = self._policy(env)
 
             if len(available) > 0:
-                # Action probabilities need to be created as a one-hot vector
                 action_probs = np.zeros_like(action_probs)
-                leaf_value_ = leaf_value.cpu().mean(axis=0).squeeze()
 
+                # leaf_value_: shape = (n_actions,)
+                if self.rl_model == "DQN":
+                    leaf_value_ = leaf_value.cpu().numpy()
+                else:  # QRDQN
+                    leaf_value_ = leaf_value.cpu().mean(axis=0).squeeze()
+
+                # mask out illegal actions
                 masked_leaf_value = np.zeros_like(leaf_value_)
                 masked_leaf_value[available] = leaf_value_[available]
 
-                # In the case of QRDQN, the shape of leaf_value is (batch, n_quantiles, n_actions),
-                # so the average needs to be taken over the quantiles.
+                # greedy action selection among available actions
                 idx_max = available[np.argmax(masked_leaf_value[available])]
                 action_probs[idx_max] = 1
 
-                # add epsilon to sensible moves and discount as much as it from the action_probs[idx_max]
-                action_probs[available] += self.epsilon / len(available)
-                action_probs[idx_max] -= self.epsilon
-
-                """ use bellman optimality to cal state value """
+                # bellman optimality: max Q-value
                 leaf_value = masked_leaf_value[available].max()
                 action_probs = zip(available, action_probs[available])
+
             else:
-                # Even if len(available) == 0, there is no issue because the leaf value will eventually be set to 0.
-                leaf_value = leaf_value.cpu().mean(axis=0).max()
+                if self.rl_model == "DQN":
+                    leaf_value = leaf_value.cpu().numpy().max()
+                    """ use oracle """
+                    """ calculate next node.select's node._Q """
+                    # leaf_temp = node._Q + (leaf_value - node._Q)/ (node._n_visits+1)
+                    # # do we need to calculate next node._u?
+                    # leaf_value = leaf_value_[leaf_temp.argmax()]
+
+                    """ use bellman expection to cal state value """
+                    # leaf_value = (leaf_value_*action_probs).mean()
+                else:
+                    leaf_value = leaf_value.cpu().mean(axis=0).max()
                 action_probs = zip(available, action_probs[available])
+
+
+        # if self.rl_model in "DQN":
+        #     available, action_probs, leaf_value = self._policy(env)
+        #     if len(available) > 0:
+        #         action_probs = np.zeros_like(action_probs)
+        #
+        #         leaf_value_ = leaf_value.cpu().numpy()
+        #         masked_leaf_value = np.zeros_like(leaf_value_)
+        #         masked_leaf_value[available] = leaf_value_[available]
+        #
+        #         idx_max = available[np.argmax(masked_leaf_value[available])]
+        #         action_probs[idx_max] = 1
+        #
+        #         """ use bellman optimality to cal state value """
+        #         leaf_value = masked_leaf_value[available].max()
+        #         action_probs = zip(available, action_probs[available])
+        #
+        #         """ use oracle """
+        #         """ calculate next node.select's node._Q """
+        #         # leaf_temp = node._Q + (leaf_value - node._Q)/ (node._n_visits+1)
+        #         # # do we need to calculate next node._u?
+        #         # leaf_value = leaf_value_[leaf_temp.argmax()]
+        #
+        #         """ use bellman expection to cal state value """
+        #         # leaf_value = (leaf_value_*action_probs).mean()
+        #     else:
+        #         leaf_value = leaf_value.cpu().numpy().max()
+        #         action_probs = zip(available, action_probs[available])
+        #
+        # elif self.rl_model == "QRDQN":
+        #     available, action_probs, leaf_value = self._policy(env)
+        #
+        #     if len(available) > 0:
+        #         action_probs = np.zeros_like(action_probs)
+        #         leaf_value_ = leaf_value.cpu().mean(axis=0).squeeze()
+        #
+        #         masked_leaf_value = np.zeros_like(leaf_value_)
+        #         masked_leaf_value[available] = leaf_value_[available]
+        #
+        #         idx_max = available[np.argmax(masked_leaf_value[available])]
+        #         action_probs[idx_max] = 1
+        #
+        #         leaf_value = masked_leaf_value[available].max()
+        #         action_probs = zip(available, action_probs[available])
+        #     else:
+        #         leaf_value = leaf_value.cpu().mean(axis=0).max()
+        #         action_probs = zip(available, action_probs[available])
 
         else:  # state version AC, QRAC, QAC, QRQAC
             available, action_probs, leaf_value = self._policy(env)
@@ -257,6 +282,8 @@ class MCTSPlayer(object):
     def __init__(self, policy_value_function, args, is_selfplay=0):
         self.mcts = MCTS(policy_value_function, args)
         self._is_selfplay = is_selfplay
+        self.rl_model = args.rl_model
+        self.epsilon = args.epsilon
         self.elo = 1500
 
     def set_player_ind(self, p):
@@ -273,11 +300,18 @@ class MCTSPlayer(object):
             acts, probs, pd, nq = self.mcts.get_move_probs(env, temp)  # env.state_.shape = (5,9,4)
             move_probs[list(acts)] = probs
             if self._is_selfplay:
-                # add Dirichlet Noise for exploration (needed for self-play training)
-                move = np.random.choice(
-                    acts,
-                    p=0.75 * probs + 0.25 * np.random.dirichlet(0.3 * np.ones(len(probs)))
-                )
+                if self.rl_model in ["DQN", "QRDQN"]:
+                    # epsilon-greedy exploration for value-based methods
+                    if np.random.rand() < self.epsilon:
+                        move = np.random.choice(acts)
+                    else:
+                        move = acts[np.argmax(probs)]
+                else:
+                    # Dirichlet noise for policy-based methods
+                    move = np.random.choice(
+                        acts,
+                        p=0.75 * probs + 0.25 * np.random.dirichlet(0.3 * np.ones(len(probs)))
+                    )
                 # update the root node and reuse the search tree
                 self.mcts.update_with_move(move)
             else:
